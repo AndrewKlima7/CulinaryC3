0866802ce23cb76c7418c539433c98359b2b3303
Maggie Tamanini <82274835+teiggam@users.noreply.github.com>
2021-08-11 15:34:00 -0400

    Fixing issues for publishing

--- CulinaryC3/Startup.cs
+++ CulinaryC3/Startup.cs
@@ -63,12 +63,6 @@ namespace CulinaryC3
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
-                RequestPath = new PathString("/Resources")
-            });
-
 
             if (!env.IsDevelopment())
             {
